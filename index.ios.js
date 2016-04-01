3c40b449a3396eb218a14866b9db9ce6b793a660
Chiara-yen <chiyen.chiara@gmail.com>
2016-04-01 17:13:30 +0800

    add Movie component

--- index.ios.js
+++ index.ios.js
@@ -10,6 +10,7 @@ import React, {
   Text,
   View
 } from 'react-native';
+import Movie from './components/Movie.js';
 
 class AwesomeProject extends Component {
   render() {
@@ -18,24 +19,7 @@ class AwesomeProject extends Component {
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-        <View style={styles.container}>
-          <Text style={styles.welcome}>
-            next
-          </Text>
-          <Text style={styles.instructions}>
-            test
-          </Text>
-          <Text style={styles.instructions}>
-            click me!
-          </Text>
-        </View>
+        <Movie />
       </View>
     );
   }
@@ -43,21 +27,13 @@ class AwesomeProject extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 20,
     color: '#F69',
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
