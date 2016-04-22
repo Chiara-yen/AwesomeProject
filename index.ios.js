05a7a878d6d10ce8392a76bb610b4e6316d69a57
Chiara-yen <chiyen.chiara@gmail.com>
2016-04-22 14:38:29 +0800

    add react-native-youtube into index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,41 +1,60 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-
 import React, {
   AppRegistry,
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity
 } from 'react-native';
 
+import YouTube from 'react-native-youtube';
+
 class AwesomeProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+      status: null,
+      quality: null,
+      error: null,
+      isPlaying: true
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          {"<Youtube /> component for \n React Native."}
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          http://github.com/paramaggarwal/react-native-youtube
         </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-        <View style={styles.container}>
-          <Text style={styles.welcome}>
-            next
-          </Text>
-          <Text style={styles.instructions}>
-            test
-          </Text>
-          <Text style={styles.instructions}>
-            click me!
-          </Text>
-        </View>
+
+        <YouTube
+          videoId="KVZ-P-ZI6W4" // React.js Conf video
+          // videoId="AuqvEYiMSeE" // Disney film - Boots
+          // videoId="PZKpSsvMRxU" // Journey soundtrack
+          // videoId="yujmvVtUh9A" // <<I Took A Pill In Ibiza>> restricted from playback => VEVO
+          // videoId="HJVW42s3vrk" // <<Try Everything>> restricted from playback => UMG
+          play={this.state.isPlaying}
+          hidden={false}
+          playsInline={true}
+          onReady={(e)=>{this.setState({isReady: true})}}
+          onChangeState={(e)=>{this.setState({status: e.state})}}
+          onChangeQuality={(e)=>{this.setState({quality: e.quality})}}
+          onError={(e)=>{this.setState({error: e.error})}}
+          style={{alignSelf: 'stretch', height: 300, backgroundColor: 'black', marginVertical: 10}}
+        />
+
+        <TouchableOpacity onPress={()=>{this.setState((s) => {return {isPlaying: !s.isPlaying};} )}}>
+          <Text style={[styles.welcome, {color: 'blue'}]}>{this.state.status == 'playing' ? 'Pause' : 'Play'}</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.instructions}>{this.state.isReady ? 'Player is ready.' : 'Player setting up...'}</Text>
+        <Text style={styles.instructions}>Status: {this.state.status}</Text>
+        <Text style={styles.instructions}>Quality: {this.state.quality}</Text>
+        <Text style={styles.instructions}>{this.state.error ? 'Error: ' + this.state.error : ' '}</Text>
       </View>
     );
   }
@@ -52,7 +71,6 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-    color: '#F69',
   },
   instructions: {
     textAlign: 'center',
